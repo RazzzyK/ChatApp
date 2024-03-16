77e7ed8574a0ace132121185bbcd193344b80006
RazzzyK <Razenok@yahoo.com>
2024-03-16 02:18:59 -0400

    Initial Commit

---
+++ ChatApp/Controllers/ChatController.cs
@@ -0,0 +1,43 @@
+﻿using ChatApp.Models;
+using ChatApp.Persistance;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Windows;
+
+namespace ChatApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatController : ControllerBase
+    {
+        private readonly YourDbContext _context;
+        private readonly IConfiguration _configuration; //remove
+
+        public ChatController(YourDbContext context, IConfiguration configuration) {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult GetUserName()
+        {
+            User user1 = new User(); 
+            
+            user1.Username = "JLai";
+            user1.Password = "password";
+
+            return Ok(user1);
+        }
+
+        [HttpPost]
+        public IActionResult Register(User u) {
+
+            Console.Write(u.Username);  
+            
+            _context.Users.Add(u);
+            _context.SaveChanges();
+
+            return Ok("User Saved Sucessfully");
+        }
+    }
+}
---
+++ ChatApp/Persistance/YourDbContext.cs
@@ -0,0 +1,41 @@
+﻿using ChatApp.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+
+namespace ChatApp.Persistance
+{
+    public class YourDbContext : DbContext
+    {
+        
+        public DbSet<User> Users { get; set; }
+
+        private readonly IConfiguration _configuration;
+
+        public YourDbContext(DbContextOptions<YourDbContext> options, IConfiguration configuration) : base(options)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        public bool IsDatabaseConnected()
+        {
+            try
+            {
+                //Database.EnsureCreated(); // Ensure that the database is created (optional)
+                Database.OpenConnection(); // Open a connection to the database
+                Database.CloseConnection(); // Close the connection
+                return true; // Connection is successful
+            }
+            catch
+            {
+                return false; // Connection failed
+            }
+        }
+
+    }
+}
